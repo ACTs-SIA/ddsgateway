f9f1e7ef32d093c77e9805f7f7a1ee0700b07043
khai <tagun.kyla11@gmail.com>
2026-04-08 05:11:53 +0800

    Updated

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class Movie extends Model
 {
+    // If you want to use the Site 2 database:
     protected $connection = 'mysql_site2'; 
 
     protected $table = 'movies';
-
-    protected $fillable = [
-        'title',
-        'genre',
-    ];
+    protected $fillable = ['title', 'genre'];
 }
--- config/database.php
+++ config/database.php
@@ -1,21 +1,25 @@
-'connections' => [
-    'mysql' => [ // This is your 'site1'
-        'driver'    => 'mysql',
-        'host'      => env('DB_HOST'),
-        'port'      => env('DB_PORT'),
-        'database'  => env('DB_DATABASE'),
-        'username'  => env('DB_USERNAME'),
-        'password'  => env('DB_PASSWORD'),
-        // ...
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [ // Site 1
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST'),
+            'port'      => env('DB_PORT'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+        ],
+        'mysql_site2' => [ // Site 2
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST_SITE2'),
+            'port'      => env('DB_PORT_SITE2'),
+            'database'  => env('DB_DATABASE_SITE2'),
+            'username'  => env('DB_USERNAME_SITE2'),
+            'password'  => env('DB_PASSWORD_SITE2'),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+        ],
     ],
-
-    'mysql_site2' => [ // Explicitly map the Site 2 env vars
-        'driver'    => 'mysql',
-        'host'      => env('DB_HOST_SITE2'),
-        'port'      => env('DB_PORT_SITE2'),
-        'database'  => env('DB_DATABASE_SITE2'),
-        'username'  => env('DB_USERNAME_SITE2'),
-        'password'  => env('DB_PASSWORD_SITE2'),
-        // ...
-    ],
-],
+];
